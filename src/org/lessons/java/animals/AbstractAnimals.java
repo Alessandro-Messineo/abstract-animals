de5901b607984ffc837105b505e6f35f7504c283
AlessandroMessineo <alessandromessineo46@gmail.com>
2026-01-09 15:40:45 +0100

    classe AbstractAnimals

---
+++ src/org/lessons/java/animals/AbstractAnimals.java
@@ -0,0 +1,18 @@
+package org.lessons.java.animals;
+
+
+// creazione classe astratta
+public abstract class AbstractAnimals {
+
+    // metodo normale
+    public void dormi(){
+        System.out.println("zzz");
+    }
+
+    // metodo astratto verso
+    public abstract void verso();
+
+    //meodo astrattp mangia
+    public abstract void mangia();
+
+}
