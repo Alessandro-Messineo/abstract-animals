8bc1174b22e3159dc5006194cca82ef265d4d325
AlessandroMessineo <alessandromessineo46@gmail.com>
2026-01-09 15:58:07 +0100

    classe main per testare

---
+++ src/org/lessons/java/animals/Main.java
@@ -0,0 +1,20 @@
+package org.lessons.java.animals;
+
+public class Main {
+    public static void main(String[] args) {
+
+        AbstractAnimals c1 = new Cane();
+        Cane c2 = new Cane();
+
+        Delfino d1 = new Delfino();
+
+        c1.verso();
+        c2.verso();
+
+        c1.mangia();
+        c1.dormi();
+
+        d1.verso();
+
+    }
+}
